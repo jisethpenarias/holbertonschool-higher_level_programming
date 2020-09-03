f12d5038b9f9dd63dca28551f86883a69c83ebbc
jpenariasholberton <1875@holbertonschool.com>
2020-09-02 19:08:17 -0500

    fix pep8

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,5 +7,5 @@ def fizzbuzz():
             print("Fizz", end=" ")
         elif number % 5 == 0:
             print("Buzz", end=" ")
-        else: 
+        else:
             print(number, end=" ")
