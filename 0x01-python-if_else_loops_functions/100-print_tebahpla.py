9e23b805dfe77269e6f7d38ca35a3f370d8f4c0f
jpenariasholberton <1875@holbertonschool.com>
2020-09-02 20:03:33 -0500

    adding format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for leter in range(122, 96, -1):
-    print(chr(leter) if leter % 2 == 0 else chr(leter-32), end="")
+    print("{}".format(chr(leter) if leter % 2 == 0 else chr(leter-32)), end="")
