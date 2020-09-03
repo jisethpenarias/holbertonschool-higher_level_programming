8e6f9c05965606914b0d59a87c02f6c0c135d53c
jpenariasholberton <1875@holbertonschool.com>
2020-09-03 15:42:13 -0500

    modifying function names in print

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
 
     print("{} + {} = {}".format(a, b, add(a, b)))
 
-    print("{} - {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
 
-    print("{} * {} = {}".format(a, b, add(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
 
-    print("{} % {} = {}".format(a, b, add(a, b)))
+    print("{} % {} = {}".format(a, b, div(a, b)))
