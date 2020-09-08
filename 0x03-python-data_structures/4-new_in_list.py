7bf6bc5a615b09cd8f7bafc435d1de7d53662ceb
jpenariasholberton <1875@holbertonschool.com>
2020-09-08 18:42:33 -0500

    replacing an element without modifying the origin

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copyList = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    copyList[idx] = element
+    return copyList
