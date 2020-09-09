3a0f9fab6e3c0a8087c7eeb120ceb98a4dab11b2
jpenariasholberton <1875@holbertonschool.com>
2020-09-08 20:41:18 -0500

    fix pep8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     for axis1 in matrix:
         for idx, axis2 in enumerate(axis1):
-            print("{:d}".format(axis2), end="" if idx == len(axis1) - 1 else " ")
+            if idx == len(axis1) - 1:
+                print("{:d}".format(axis2), end="")
+            else:
+                print("{:d}".format(axis2), end=" ")
         else:
             print("\n", end="")
