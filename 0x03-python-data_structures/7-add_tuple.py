d0f2be1190224bd0f28ce55dd7048487a21cbf7e
jpenariasholberton <1875@holbertonschool.com>
2020-09-09 11:40:55 -0500

    fix pep8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,4 +2,4 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     d_a = tuple_a + (0, 0)
     d_b = tuple_b + (0, 0)
-  return d_a[0] + d_b[0], d_a[1] + d_b[1]
+    return d_a[0] + d_b[0], d_a[1] + d_b[1]
