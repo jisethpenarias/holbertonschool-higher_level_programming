76c2b151fe5a2f340995570d847e7cf9975a8207
jpenariasholberton <1875@holbertonschool.com>
2020-09-08 20:38:32 -0500

    adding decimal format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for axis1 in matrix:
         for idx, axis2 in enumerate(axis1):
-            print("{}".format(axis2), end="" if idx == len(axis1) - 1 else " ")
+            print("{:d}".format(axis2), end="" if idx == len(axis1) - 1 else " ")
         else:
             print("\n", end="")
