b073c871c8ac328fd3ef38f7ab9cadc20d28c165
jpenariasholberton <1875@holbertonschool.com>
2020-09-08 20:21:11 -0500

    prints a matrix of integers.

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+  for axis1 in matrix:
+    for axis2 in axis1:
+      print("{}".format(axis2), end=" ")
+    else:
+      print("\n", end="")
