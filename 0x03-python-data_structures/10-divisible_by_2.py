ca14d59876c1e7355b431b6611a17208102baf5d
jpenariasholberton <1875@holbertonschool.com>
2020-09-09 10:10:57 -0500

    all multiples of 2 in a list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    mult = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            mult.append(True)
+        else:
+            mult.append(False)
+    return mult
