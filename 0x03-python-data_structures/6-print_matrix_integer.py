794de5a02f03791dde9fed712797bfd7c4420c46
jpenariasholberton <1875@holbertonschool.com>
2020-09-08 20:36:27 -0500

    adding condition whit index

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for axis1 in matrix:
-        for axis2 in axis1:
-            print("{}".format(axis2), end=" ")
+        for idx, axis2 in enumerate(axis1):
+            print("{}".format(axis2), end="" if idx == len(axis1) - 1 else " ")
         else:
             print("\n", end="")
