ebbb6ab50bce7c5d212dfe7211f97572c10837f4
jpenariasholberton <1875@holbertonschool.com>
2020-09-11 17:35:46 -0500

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,5 @@
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
+        return (max(a_dictionary, key=a_dictionary.get))
+    else:
+        return (None)
