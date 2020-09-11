89b3f0b4cb7fd814b58771c682f4006bf45229aa
jpenariasholberton <1875@holbertonschool.com>
2020-09-10 23:03:57 -0500

    change condition

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for value in sorted(a_dictionary.items()):
-        print ("{}: {}".format(value[0], value[1]))
+    for k, value in sorted(a_dictionary.items()):
+        print ("{}: {}".format(k, value))
