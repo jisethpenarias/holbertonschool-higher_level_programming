f97c54a57e8926d7359ed6ab89339466c0fcfa19
jpenariasholberton <1875@holbertonschool.com>
2020-09-21 18:11:10 -0500

    fix pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     num = 0
     for element in my_list[0:x]:
-        try: 
+        try:
             print("{}".format(my_list[element]), end="")
             num += 1
         except:
