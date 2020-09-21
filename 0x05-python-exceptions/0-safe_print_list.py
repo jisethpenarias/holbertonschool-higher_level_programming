7249860b711929207b73cd047e2e09e5eb2b8372
jpenariasholberton <1875@holbertonschool.com>
2020-09-21 18:22:54 -0500

    change condition print line 6

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     num = 0
     for element in my_list[0:x]:
         try:
-            print("{}".format(my_list[element]), end="")
+            print("{}".format(element),end="")
             num += 1
         except:
             break
