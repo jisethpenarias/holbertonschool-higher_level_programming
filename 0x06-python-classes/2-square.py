b65c4b986b78ff21c4f63fa285ff9d00c1fba005
jpenariasholberton <1875@holbertonschool.com>
2020-09-22 20:42:50 -0500

    defines a square using raise exception

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""0-square module"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """class builder"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
