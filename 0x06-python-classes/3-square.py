72caa14eb4bdcd968fe64f4e96101a144e18f8a7
jpenariasholberton <1875@holbertonschool.com>
2020-09-22 20:53:03 -0500

    Public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""0-square module"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """class builder"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """instance method"""
+        return self.__size ** 2
