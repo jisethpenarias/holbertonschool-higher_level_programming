16b1d2cb048fc3e1daaa210e8cfeb3e213a1d7ad
jpenariasholberton <1875@holbertonschool.com>
2020-09-22 23:40:03 -0500

    change method print if for "for"

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,8 +34,8 @@ class Square:
         if self.__size == 0:
             print("")
         else:
-            if self.__position[1] > 0:
-                print("")
+            for y in range(self.__position[1]):
+                print()
             for i in range(self.__size):
                 for space in range(self.__position[0]):
                     print(" ", end="")
