49ec00f798656143b45ae9ef3e3cc658d4dc7042
jpenariasholberton <1875@holbertonschool.com>
2020-09-28 18:47:58 -0500

    fix message valueError

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
