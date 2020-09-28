b0e3be6deb7db5b18a373e87e09a76975365264e
jpenariasholberton <1875@holbertonschool.com>
2020-09-28 18:39:03 -0500

    editing value 2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
         """setter method width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value_width < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__width = value
 
