3acdff67657886f6c54e26ea176bb89c11db4170
jpenariasholberton <1875@holbertonschool.com>
2020-09-28 21:01:43 -0500

    bigger_or_equal area()

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -82,3 +82,16 @@ class Rectangle:
     def __del__(self):
         print('Bye rectangle...')
         Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() == rect_2.area():
+            return rect_1
+        elif rect_1.area() > rect_2.area():
+            return rect_1
+        else:
+            return rect_2
