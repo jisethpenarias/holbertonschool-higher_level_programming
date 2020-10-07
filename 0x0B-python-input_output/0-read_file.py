7f630b6063eb9c85c88ec80cb9a9e17f2c7429a7
jpenariasholberton <1875@holbertonschool.com>
2020-10-07 17:02:02 -0500

    adding encoding="utf-8"

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def read_file(filename=""):
+def read_file(filename="", encoding='utf-8'):
     with open(filename) as readFile:
         read_data = readFile.read()
         print(read_data, end="")
