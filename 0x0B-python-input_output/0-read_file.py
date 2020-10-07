91c96e503033e55c15cc1daf4e81ea6529b3e1e5
jpenariasholberton <1875@holbertonschool.com>
2020-10-07 16:59:35 -0500

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
+
 def read_file(filename=""):
-    with open("my_file_0.txt") as readFile:
-        for line in readFile:
-            print(line, end="")
+    with open(filename) as readFile:
+        read_data = readFile.read()
+        print(read_data, end="")
+        readFile.closed
