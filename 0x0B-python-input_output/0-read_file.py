7daed28cf670e3684938e54fdd3c8760b88de74c
jpenariasholberton <1875@holbertonschool.com>
2020-10-07 17:06:34 -0500

    adding documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""function that reads a text file"""
 
 
-def read_file(filename="", encoding='utf-8'):
+def read_file(filename="", encoding="utf-8"):
+    """function that reads a text file (UTF8) and prints it to stdout"""
+
     with open(filename) as readFile:
         read_data = readFile.read()
         print(read_data, end="")
