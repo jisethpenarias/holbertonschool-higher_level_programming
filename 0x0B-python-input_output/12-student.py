877219ce103c532b624e5dffe21a200f2c6fc75b
jpenariasholberton <1875@holbertonschool.com>
2020-10-07 23:26:44 -0500

    retrieves a dictionary

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method that retrieves a dictionary
+        representation of a Student instance"""
+
+        if attrs is None:
+            return self.__dict__
+
+        flag_instance = 0
+        for atribute in attrs:
+            if not isinstance(atribute, str):
+                flag_instance = 1
+
+        if flag_instance == 0:
+            dic_to_return = {}
+            for key in attrs:
+                try:
+                    dic_to_return[key] = self.__dict__[key]
+                except:
+                    pass
+            return dic_to_return
+
+        return self.__dict__
