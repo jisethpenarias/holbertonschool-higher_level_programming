d79efe2ecf11df9981d8238058c756e8f70a8a33
jpenariasholberton <1875@holbertonschool.com>
2020-10-13 16:51:28 -0500

    delete variable in from_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -70,8 +70,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
-        list_empaty = len(json_string) == 0
-        if json_string is None or json_string is list_empaty:
+        if json_string is None or json_string is (len(json_string) == 0):
             return []
         else:
             return json.loads(json_string)
