a724d2f3d5f5fba7bcca276622e316a636c13c5e
jpenariasholberton <1875@holbertonschool.com>
2020-11-16 19:08:54 -0500

    adding (if nor exists)

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
 -- creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE hbtn_0c_0;
+-- IF NOT EXISTS: is added in case the database exists no error occurs.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
