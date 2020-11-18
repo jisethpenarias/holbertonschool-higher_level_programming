b6e7c7020362bf18aeb2732e3289bc0947b23552
jpenariasholberton <1875@holbertonschool.com>
2020-11-17 20:35:26 -0500

    specify database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 --  lists all cities contained in the database hbtn_0d_usa.
 
 SELECT cities.id, cities.name, states.name
-FROM cities 
-    INNER JOIN states USING (id)
+FROM hbtn_0d_usa.cities 
+    INNER JOIN hbtn_0d_usa.states ON cities.state_id = states.id
 ORDER BY cities.id ASC
