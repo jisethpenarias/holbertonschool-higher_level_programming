6f048b573fb2177902ceda485084f77ddc1605e2
jpenariasholberton <1875@holbertonschool.com>
2020-11-17 20:37:10 -0500

    change it by Left

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 
 SELECT cities.id, cities.name, states.name
 FROM hbtn_0d_usa.cities 
-    INNER JOIN hbtn_0d_usa.states ON cities.state_id = states.id
+    LEFT JOIN hbtn_0d_usa.states ON cities.state_id = states.id
 ORDER BY cities.id ASC
