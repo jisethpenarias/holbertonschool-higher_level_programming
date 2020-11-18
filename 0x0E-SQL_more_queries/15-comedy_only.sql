c1490a6feb481a46a9a992bb62f51d301239eb09
jpenariasholberton <1875@holbertonschool.com>
2020-11-17 22:23:34 -0500

    titles

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- Results must be sorted in ascending order by the show title
+
+SELECT  tv_shows.title
+FROM    tv_genres
+        INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+        INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE   tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC;
