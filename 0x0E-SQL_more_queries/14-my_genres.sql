9b948b46ad2d909eeecd522c7b51acd150f11fd9
jpenariasholberton <1875@holbertonschool.com>
2020-11-17 22:19:58 -0500

    double inner

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Results must be sorted in ascending order by the genre name
+
+SELECT  tv_genres.name
+FROM    tv_shows    
+        INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+        INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE   tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
