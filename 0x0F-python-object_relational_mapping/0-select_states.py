da989885da67c5f6b8effdf008235064749e1560
jpenariasholberton <1875@holbertonschool.com>
2020-12-13 14:12:07 -0500

    closing SQL statement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
     rows = cur.fetchall()
     for row in rows:
         print (row)
