46989e6f124f6038d1a342dab62fe63213755568
jpenariasholberton <1875@holbertonschool.com>
2020-12-13 14:05:50 -0500

    comments removed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,12 +14,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=db_name)
     cur = db.cursor()
-    # cur.execute return the number of states
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print (row)
-    # Close all cursors
     cur.close()
-    # Close all databases
     db.close()
