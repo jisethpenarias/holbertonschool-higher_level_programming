5b84d4badfb923b25b998b571033ca85cac08e45
jpenariasholberton <1875@holbertonschool.com>
2020-12-13 14:36:27 -0500

    delete spaces line 20

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print (row)
+        print(row)
     cur.close()
     db.close()
