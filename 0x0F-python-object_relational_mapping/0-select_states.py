42734ea759a1d5b3dd7b76cd7091f8418f5ffe9e
jpenariasholberton <1875@holbertonschool.com>
2020-12-13 14:08:58 -0500

    docs modified

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
-
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     username = sys.argv[1]
