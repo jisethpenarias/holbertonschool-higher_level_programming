6683d02fed44ed40a8b4ae1f111281468d44626e
jpenariasholberton <1875@holbertonschool.com>
2020-12-18 20:23:27 -0500

    removing comments

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # get status code response
-# --silent Dont show progress or error messages
-# --location if a page was moved to a different location 
-# this will re do the curl to the right location
-# --head just get the headers of the response
-# --output file where you want the output
-# --write-out  the format of the output in this case we only need the http code
 curl --silent --location --head "$1" --output /dev/null --write-out '%{http_code}'
