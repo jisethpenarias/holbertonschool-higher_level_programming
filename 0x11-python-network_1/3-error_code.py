f90c84eb635ed0d985b5804178852b546df67d18
jpenariasholberton <1875@holbertonschool.com>
2021-01-11 17:07:28 -0500

    Error code: displays the body of the response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Error code: script that takes in a URL,
+sends a request to the URL and displays the body
+of the response (decoded in utf-8). """
+
+import urllib.request
+import urllib.error
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    try: 
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as error_code:
+        print('Error code:', error_code.code)
