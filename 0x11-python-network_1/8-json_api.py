afd67367c2b87bfd7d808af6fcdada279b08d2e5
jpenariasholberton <1875@holbertonschool.com>
2021-01-11 19:35:14 -0500

    adding try cases

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" """
+""" Search API: script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter."""
 
 import requests
 from sys import argv
@@ -10,10 +11,11 @@ if __name__ == "__main__":
         q = argv[1]
 
     r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
-    jR = r.json()
-    if bool(jR):
-        print("[<{}>] <{}>".format(jR.get('id'), jR.get('name')))
-    elif not bool(jR):
-        print("No result")
-    else:
+    try:
+        jR = r.json()
+        if bool(jR):
+            print("[<{}>] <{}>".format(jR.get('id'), jR.get('name')))
+        else:
+            print("No result")
+    except:
         print("Not a valid JSON")
