8ebcb1468e437051b0fbb9fab6b893538b8d6c06
jpenariasholberton <1875@holbertonschool.com>
2021-02-02 21:27:27 -0500

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,4 @@
 /* script that searches the second biggest
 integer in the list of arguments. */
 
-const number = process.argv;
-
-if (number.length <= 3) {
-  console.log(0);
-} else {
-  const numSecondBig = number.sort(function compare (a, b) { return b - a; });
-  console.log(numSecondBig[3]);
-}
+console.log(process.argv.length <= 3 ? 0 : process.argv.sort(function compare (a, b) { return b - a; })[3]);
