ff5a80ddfb0ad6af46da36bdd4338eebf75e7e9e
jpenariasholberton <1875@holbertonschool.com>
2021-02-02 21:12:55 -0500

    searches the second biggest integer

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* script that searches the second biggest
+integer in the list of arguments. */
+
+const number = process.argv;
+
+if (number.length <= 3) {
+  console.log(0);
+} else {
+  const numSecondBig = number.sort(function compare (a, b) { return b - a; });
+  console.log(numSecondBig[3])
+}
