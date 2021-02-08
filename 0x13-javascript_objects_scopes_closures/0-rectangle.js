ef8e5839aef8d511982b689a707e6aaf0a10fbfa
jpenariasholberton <1875@holbertonschool.com>
2021-02-08 18:55:54 -0500

    Empty class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Empty class Rectangle that defines a rectangle
+class Rectangle {
+}
+/* The constructor is a method of the class used to set the properties
+of the object or to call methods to prepare the object for use */
+module.exports = Rectangle;
