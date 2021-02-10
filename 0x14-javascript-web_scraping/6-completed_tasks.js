95c8223a5ba0f729939124e0409a74d30e58be19
jpenariasholberton <1875@holbertonschool.com>
2021-02-09 21:30:19 -0500

    computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,25 +6,22 @@ const apiUrl = process.argv[2];
 
 request.get(apiUrl, response);
 
-function response (error, response, body) {
-  if (error) {
-    console.log(error);
+function response (err, response, body) {
+  if (err) {
+    console.log(err);
   } else {
-    const userIds = [];
     const completedTasks = {};
     const bodyJson = JSON.parse(body);
     // console.log(body);
-    bodyJson.forEach(element => {
-      if (!userIds.includes(element.userId)) {
-        userIds.push(element.userId);
-      }
-    });
-    userIds.forEach(id => {
-      completedTasks[id] = bodyJson
-        .filter(task => task.userId === id && task.completed)
-        .map(task => 1)
-        .reduce((acc, cur) => acc + cur);
-    });
+    bodyJson
+      .filter(task => task.completed)
+      .forEach(task => {
+        if (!completedTasks[task.userId]) {
+          completedTasks[task.userId] = 1;
+        } else {
+          completedTasks[task.userId] += 1;
+        }
+      });
     console.log(completedTasks);
   }
 }
