948ad5b9e4cb839ade45a3ee910fdb3f49c31c7b
jpenariasholberton <1875@holbertonschool.com>
2021-02-10 17:53:53 -0500

    adds a LI element to a list when the user clicks on the tag specific

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,9 @@
+/* adds a LI element to a list when the user clicks on the tag DIV#add_item:
+The new element must be: <li>Item</li>
+The new element must be added to UL.my_list */
+
+document.addEventListener('DOMContentLoaded', function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+});
