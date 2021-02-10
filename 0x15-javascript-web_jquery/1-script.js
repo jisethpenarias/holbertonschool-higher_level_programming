46a98c58901eb79c330e540a19c958227034d947
jpenariasholberton <1875@holbertonschool.com>
2021-02-10 17:40:24 -0500

    updates the text color of the HTML tag HEADER

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+/* updates the text color of the HTML tag HEADER to red
+no use document.querySelector */
+
+document.addEventListener('DOMContentLoaded', function () {
+  $('HEADER').css('color', '#FF0000');
+});
